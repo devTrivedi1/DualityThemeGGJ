8d7bcf22546aad7e20e14c5515007a98a2ff98e1
Dev <d.trivedi@saedubai.com>
2022-01-27 14:51:16 +0400

    Added jump gravity and some minor tweaks

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,13 +8,19 @@ public class Movement : MonoBehaviour
 	private float veritcalMovement;
 
 	[SerializeField] private float moveSpeed;
+	
 
 	private Rigidbody2D playerRB;
 
-	private Vector2 currentVelocity;
 
+	private bool hasJumped;
+	[SerializeField]private bool isGrounded = true;
+
+	private Vector2 currentVelocity;
+	[SerializeField] float jumpForce;
 	void Start()
 	{
+		isGrounded = GameObject.FindGameObjectWithTag("Ground");
 		playerRB = GetComponent<Rigidbody2D>();
 	}
 	void Update()
@@ -24,13 +30,26 @@ public class Movement : MonoBehaviour
 	void FixedUpdate()
 	{
 		MovePlayer();
+		if(hasJumped)
+		{
+			Jump();
+			hasJumped = false;
+			isGrounded = false;
+		}
 	}
 
 	void ReadInputs()
 	{
+		if(Input.GetButtonDown("Jump") && isGrounded == true)
+		{
+			hasJumped = true;
+		}
+
 		horizontalMovement = Input.GetAxis("Horizontal");
 		veritcalMovement = Input.GetAxis("Vertical");
+
 		currentVelocity = this.playerRB.velocity;
+
 	}
 
 	void MovePlayer()
@@ -38,6 +57,22 @@ public class Movement : MonoBehaviour
 		if (horizontalMovement != 0)
 		{
 			playerRB.velocity = new Vector2(horizontalMovement * moveSpeed, currentVelocity.y);
+		}
+	}
+	
+	void Jump()
+	{
+		if(hasJumped && isGrounded)
+		{
+			playerRB.velocity = Vector2.up * jumpForce;
+		}
+	
+	}
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if(other.gameObject.CompareTag("Ground"))
+		{
+			isGrounded = true;	
 		}
 	}
 }
